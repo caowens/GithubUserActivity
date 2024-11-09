a7d94dad3e02d3089f9dc8c57cc3014b8afc85af
caowens <caowens555@yahoo.com>
2024-11-09 12:14:54 -0800

    Add example code for fetching Github API

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace GithubUserActivity 
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            await ProcessRepositoriesAsync(client);
+        }
+
+        static async Task ProcessRepositoriesAsync(HttpClient client)
+        {
+            await using Stream stream = await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+
+            foreach (var repo in repositories ?? Enumerable.Empty<Repository>())
+            {
+                Console.WriteLine(repo.name);
+            }
+        }
+    }
+}
